eaac8db4d5359e0783938a39dee65bb677f45884
Lorenzo Frittoli <lorenzo.frittoli.ge@gmail.com>
2023-12-01 22:47:42 +0100

    Now you can move the cursor!
    Needs fixing when you go out of bounds (right/up).

--- src/editor.rs
+++ src/editor.rs
@@ -1,25 +1,44 @@
 use std::fs::{self, File, OpenOptions};
-use std::ffi::{OsStr, OsString};
+use std::ffi::OsString;
 use std::path::Path;
-use std::io::{stdin, stdout, Read};
+use std::io::{stdout, Read};
+use crossterm::ExecutableCommand;
 use crossterm::{
     execute, cursor,
     terminal::{self, Clear, ClearType},
     style::{Color, Print, ResetColor, SetForegroundColor},
-    event::{self, Event, KeyCode, KeyEvent},
+    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
 };
 
 pub struct Editor {
     file: Option<File>,
+    width: u16,
+    height: u16,
+    right_padding: u16,
 }
+
+enum Move {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 enum Command {
     Insert(char),
+    Move(Move),
     Exit,
 }
 
 impl Editor {
     pub fn new() -> Self {
-        Editor { file: None }
+        let (width, height) = terminal::size().unwrap();
+        Editor {
+            file: None,
+            width: width,
+            height: height,
+            right_padding: 8
+        }
     }
 
     pub fn open_file(&mut self, path: &Path) {
@@ -45,17 +64,33 @@ impl Editor {
         terminal::enable_raw_mode().expect("Couldn't enable raw mode");
         
         let mut contents: String = self.get_contents();
+        let mut pos: (u16, u32) = (0, 0);
+        let mut first_line: u32 = 0;
+        let mut last_line: u32 = contents.lines().count() as u32;
         
         loop {
-            self.display(&mut contents);
+            self.draw(&mut contents, &mut pos, &first_line);
             if let Some(keypress) = self.get_keypress() {
-                let command = self.keypress_to_command(&keypress);
-                match command {
-                    Command::Insert(c) => {
-                        // TODO
-                    }
+                let command_option = self.process_keypress(&keypress);
+                
+                if let Some(command) = command_option {
+                    match command {
+                        Command::Insert(c) => {
+                            // TODO
+                        }
 
-                    Command::Exit => break, 
+                        Command::Move(direction) => {
+                            match direction {
+                                Move::Up => {pos.1 = 0.max(pos.1-1)}
+                                Move::Down => {pos.1 = last_line.min(pos.1+1)},
+                                Move::Right => {pos.0 = self.width.min(pos.0+1)},
+                                Move::Left => {pos.0 = (0 as u16).max(pos.0-1)},
+                                // Move::Left => {pos.0 = 0.max(pos.0-1)},
+                            }
+                        }
+
+                    Command::Exit => break,
+                    }
                 }
             }
         }
@@ -69,15 +104,46 @@ impl Editor {
         }
     }
 
-    fn keypress_to_command(&self, keypress: &KeyEvent) -> Command {
+    fn process_keypress(&self, keypress: &KeyEvent) -> Option<Command> {
+        match keypress {
+            KeyEvent {
+                modifiers: KeyModifiers::NONE,
+                ..
+            } => return self.process_unmodified(keypress),
+
+            KeyEvent {
+                modifiers: KeyModifiers::CONTROL,
+                ..
+            } => return self.process_ctrl(keypress),
+
+            _ => return None,
+        }
+    }
+
+    fn process_unmodified(&self, keypress: &KeyEvent) -> Option<Command> {
+        match keypress {
+            KeyEvent {
+                code: KeyCode::Char(c),
+                ..
+            } => return Some(Command::Insert(*c)),
+
+            KeyEvent {code: KeyCode::Up, ..} => return Some(Command::Move(Move::Up)),
+            KeyEvent {code: KeyCode::Down, ..} => return Some(Command::Move(Move::Down)),
+            KeyEvent {code: KeyCode::Left, ..} => return Some(Command::Move(Move::Left)),
+            KeyEvent {code: KeyCode::Right, ..} => return Some(Command::Move(Move::Right)),
+
+            _ => return None,
+        }
+    }
+
+    fn process_ctrl(&self, keypress: &KeyEvent) -> Option<Command> {
         match keypress {
             KeyEvent {
                 code: KeyCode::Char('q'),
-                modifiers: event::KeyModifiers::CONTROL,
                 ..
-            } => return Command::Exit,
+            } => Some(Command::Exit),
 
-            _ => return Command::Insert('j')
+            _ => return None,
         }
     }
 
@@ -94,24 +160,42 @@ impl Editor {
         return contents;
     }
 
-    fn display(&self, content: &mut str) {
-        for (i,l) in content.lines().enumerate() {
+    fn draw(&self, content: &mut str, pos: &mut (u16, u32), first_line: &u32) {
+        let (x, y) = pos;
+        let screen_x: u16 = (*x as u16) + self.right_padding;
+        let screen_y: u16 = (*y + first_line) as u16;
+
+        self.clear_screen().unwrap();
+        for (i,l) in content
+            .lines()
+            .skip(*first_line as usize)
+            .take(self.height as usize)
+            .enumerate() {
+
             execute!(
                 stdout(),
                 SetForegroundColor(Color::Green),
                 Print(format!("{}\t", i)),
                 ResetColor,
-                Print(format!("{}\n\r", l))
+                Print(format!("{}", l)),
             ).unwrap();
+
+            if i < (self.height - 1).into() {
+                stdout().execute(Print("\n\r")).unwrap();
+            }
         }
         execute!(
             stdout(),
-            cursor::MoveTo(1,0)
+            cursor::MoveTo(screen_x, screen_y)
         ).unwrap();
     }
 
     fn clear_screen(&self) -> Result<(), std::io::Error> {
-        execute!(stdout(), Clear(ClearType::All))
+        execute!(
+            stdout(),
+            Clear(ClearType::All),
+            cursor::MoveTo(0, 0),
+        )
     }
 }
 
